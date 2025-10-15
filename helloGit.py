f8a353b70cf2fd05befcd51f578b33ebc444a424
George <trainee.george.downer@sigmalabs.co.uk>
2025-10-15 18:27:43 +0100

    Added extra to the message

--- helloGit.py
+++ helloGit.py
@@ -1,4 +1,4 @@
 from datetime import datetime
 name = 'George'
 date = datetime.strftime('%d-%m-%Y')
-print(f"Hello from {name} on {date}")
+print(f"Hello from {name} on {date}, have a wonderful day!")
