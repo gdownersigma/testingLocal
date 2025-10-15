5155a8448578e96b0554bbd320815ba35b002d06
George <trainee.george.downer@sigmalabs.co.uk>
2025-10-15 18:45:05 +0100

    Added a Vowel counter

--- text_utils.py
+++ text_utils.py
@@ -4,3 +4,11 @@ text = 'the fox ran down the road'
 def word_counter(text):
     words = text.split(' ')
     return len(words)
+
+
+def vowel_counter(text):
+    counter = 0
+    for chr in text:
+        if chr in ['a', 'e', 'i', 'o', 'u']:
+            counter += 1
+    return counter
