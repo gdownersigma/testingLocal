e4927a065483ed82a8137096fe68a353f73fb6f2
George <trainee.george.downer@sigmalabs.co.uk>
2025-10-16 11:47:32 +0100

    Fix #1: Add printing

--- calculator.py
+++ calculator.py
@@ -15,3 +15,9 @@ def divide_numbers(num1, num2):
         return num1/num2
     else:
         return 0
+
+
+print(add_numbers(7, 5))
+print(subtract_numbers(10, 2))
+print(multiply_numbers(4, 8))
+print(divide_numbers(9/3))
